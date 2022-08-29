60cffffb6d3db0a59ae89123ea738b07f11c23af
NehadAwad <nehadakash622@gmail.com>
2022-08-29 22:57:35 +0600

    backend validation added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,6 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password)
-
     res.status(200).json({email, user})
   } catch (error) {
     res.status(400).json({error: error.message})
