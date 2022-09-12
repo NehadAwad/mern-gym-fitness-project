7f21e581583f111e8467357b491355b1bdb14920
NehadAwad <nehadakash622@gmail.com>
2022-09-12 23:48:26 +0600

    server restructured

--- server/server.js
+++ server/server.js
@@ -10,25 +10,6 @@ const workoutRoutes = require('./routes/workouts.router')
 const userRoutes = require('./routes/users.router')
 
 
-//cors
-// app.use(cors());
-// app.options("", cors());
-// app.use(function (req, res, next) 
-// {
-//       res.header("Access-Control-Allow-Origin", "");
-//       res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//       res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,X-CallbackType, Content-Type, Accept");
-//       res.header("Cache-Control", "no-cache");
-//       if ("OPTIONS" == req.method) 
-//       {
-//           res.send(200);
-//       }
-//       else 
-//      {
-//        next();
-//      }
-// });
-
 //middleware
 app.use(express.json())
 app.use((req, res, next)=>{
